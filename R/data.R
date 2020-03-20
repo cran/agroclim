48147bec3f488d1f6b6b2a05532e0af373c6de72
Roberto Serrano-Notivoli <rserrano@eead.csic.es>
2020-03-20 09:30:13 +0000

    version 0.1.0

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Daily maximum  temperature
+
+#' @title Climatic dataset
+#' @description Dataset with 30 years of daily maximum temperature.
+#' @source Daily data source from the STEAD dataset of mainland Spain temperatures.
+#' @references <http://dx.doi.org/10.20350/digitalCSIC/8622>
+#' @format numeric vector with maximum temperatures in celsius degrees.
+#' \describe{
+#'   \item{\code{daily_tmax}}{numeric. daily maximum temperatures}
+#'}
+"daily_tmax"
+
+#' Daily minimum temperature
+
+#' @title Climatic dataset
+#' @description Dataset with 30 years of daily minimum temperature.
+#' @source Daily data source from the STEAD dataset of mainland Spain temperatures.
+#' @references <http://dx.doi.org/10.20350/digitalCSIC/8622>
+#' @format numeric vectors with minimumtemperatures in celsius degrees.
+#' \describe{
+#'   \item{\code{daily_tmin}}{numeric. daily minimum temperatures}
+#'}
+"daily_tmin"
